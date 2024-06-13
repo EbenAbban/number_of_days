d6619d50f12392dec41bca96c06af6f650aed618
EbenAbban <abbaneben07@gmail.com>
2024-06-13 16:48:01 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@cpp-conditions/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+ int day;
+  cin>>day;
+  switch(day)
+    case 1 :
+  cout<< "Monday";
+    case 2 :
+  cout<< "Tuesday";
+    case 3 :
+  cout<< "Wednesday";
+    case 4 :
+  cout<< "Thursday";
+    case 5 :
+  cout<< "Friday";
+    
+  
+  
+
+}
